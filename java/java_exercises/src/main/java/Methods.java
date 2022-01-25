8e75a34eacb14ec77a7e71200fb111d167a1b36c
Colin Farquhar <colin.farquhar@brightnetwork.co.uk>
2022-01-25 12:25:44 +0000

    reorder exercises

--- java/java_exercises/src/main/java/Methods.java
+++ java/java_exercises/src/main/java/Methods.java
@@ -28,13 +28,9 @@ public class Methods {
      * an argument should return -5; passing -10 should return 10.
      */
 
-    /**
-     * Exercise 3: A method which takes a string as an argument, capitalises the *first* letter and returns the
-     * capitalised string
-     */
 
     /**
-     * Exercise 4: A method which takes two numbers as arguments, checks if they are the same and returns the appropriate
+     * Exercise 3: A method which takes two numbers as arguments, checks if they are the same and returns the appropriate
      * boolean value.
      *
      * You should check *both* possible outcomes here, i.e. that it returns `true` when it is supposed to and `false` when
@@ -42,12 +38,22 @@ public class Methods {
      */
 
     /**
-     * Exercise 5: A method which takes two numbers as arguments and *prints* one of:
+     * Exercise 4: A method which takes two numbers as arguments and *prints* one of:
      * - "The first number is larger"
      * - "The second number is larger"
      * - "The numbers are equal"
      * as appropriate.
      */
+
+
+    /**
+     * Exercise 5: A method which takes a string as an argument, capitalises the *first* letter and returns the
+     * capitalised string.
+     *
+     * HINT: The .toUpperCase() method will capitalise the whole string. Investigate how to split the first letter from
+     * the string, capitalise that, then join the pieces back together
+     */
+
 
     /**
      * Exercise 6: A method which takes an array of strings as an argument and returns the number of items in the array.
@@ -55,12 +61,9 @@ public class Methods {
 
     /**
      * Exercise 7: A method which takes a sentence as an argument, counts the number of words in the array and prints
-     * "The sentence contains X words".
+     * "The sentence contains X words"
      *
-     * HINT 1: One of the methods to do this already exists within Java. Research the Java String documentation to see
-     * what you can do with strings (https://docs.oracle.com/javase/7/docs/api/java/lang/String.html)
-     *
-     * HINT 2: Remember what you've done already, you might be able to reuse one of your methods here...
+     * HINT: Remember what you've done already, you might be able to reuse one of your methods here...
      */
 
     public static void main(String[] args) {
