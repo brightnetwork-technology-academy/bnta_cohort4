fd80271e6101158550f2bf66123b4660cb97a6f3
Colin Farquhar <colin.farquhar@brightnetwork.co.uk>
2022-01-25 12:09:38 +0000

    add methods exercises

---
+++ java/java_exercises/src/main/java/Methods.java
@@ -0,0 +1,75 @@
+public class Methods {
+
+    /**
+     *
+     * Write the code for each exercise under the numbered comments.
+     *
+     * To test your code you can call the method from inside the main method. If your method should return something you
+     * can store the return value as a variable in main then print the variable.
+     *
+     */
+
+    /**
+     * EXAMPLE: A method which takes a number as an argument and multiplies it by 2. The result is printed to the
+     * terminal.
+     */
+
+    public static void multiplyNumberByTwo(int number){
+        int product = number * 2;
+        System.out.println(number + " multiplied by 2 is " + product);
+    }
+
+    /**
+     * Exercise 1: A method which takes two numbers as arguments and prints their sum to the terminal.
+     */
+
+    /**
+     * Exercise 2: A method which takes a number as an argument and returns that number negated, e.g. passing 5 as
+     * an argument should return -5; passing -10 should return 10.
+     */
+
+    /**
+     * Exercise 3: A method which takes a string as an argument, capitalises the *first* letter and returns the
+     * capitalised string
+     */
+
+    /**
+     * Exercise 4: A method which takes two numbers as arguments, checks if they are the same and returns the appropriate
+     * boolean value.
+     *
+     * You should check *both* possible outcomes here, i.e. that it returns `true` when it is supposed to and `false` when
+     * it is supposed to.
+     */
+
+    /**
+     * Exercise 5: A method which takes two numbers as arguments and *prints* one of:
+     * - "The first number is larger"
+     * - "The second number is larger"
+     * - "The numbers are equal"
+     * as appropriate.
+     */
+
+    /**
+     * Exercise 6: A method which takes an array of strings as an argument and returns the number of items in the array.
+     */
+
+    /**
+     * Exercise 7: A method which takes a sentence as an argument, counts the number of words in the array and prints
+     * "The sentence contains X words".
+     *
+     * HINT 1: One of the methods to do this already exists within Java. Research the Java String documentation to see
+     * what you can do with strings (https://docs.oracle.com/javase/7/docs/api/java/lang/String.html)
+     *
+     * HINT 2: Remember what you've done already, you might be able to reuse one of your methods here...
+     */
+
+    public static void main(String[] args) {
+        /**
+         * Call your methods on the lines below ⬇️
+         */
+        multiplyNumberByTwo(2);
+    }
+
+
+
+}
